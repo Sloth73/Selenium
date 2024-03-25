26978adc5299e4be9a84896bc195ba9262d0408d
Sloth73 <Pichl.tomas.95@gmail.com>
2024-03-25 22:26:08 +0100

    Setting initial enviroment

--- src/Section_01/SelectingByID.java
+++ src/Section_01/SelectingByID.java
@@ -1,7 +1,13 @@
 package Section_01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SelectingByID {
     public static void main(String[] args) {
-
+        System.setProperty("webdriver.driver.chrome", "/usr/local/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationtesting.co.uk/");
     }
 }
