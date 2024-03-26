5170aa0df035edff79b08180fccadacde8bc2584
Sloth73 <Pichl.tomas.95@gmail.com>
2024-03-26 11:07:32 +0100

    Selecting element by css selector

---
+++ src/Section_01/SelectingByCSS.java
@@ -0,0 +1,16 @@
+package Section_01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelectingByCSS {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.driver.chrome", "/usr/local/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationtesting.co.uk/buttons.html");
+        driver.findElement(By.cssSelector("#btn_one")).click();
+
+    }
+}
