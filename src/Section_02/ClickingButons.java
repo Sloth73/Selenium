4b5838458d5ec412c9724a646636c70380e2fb76
Sloth73 <Pichl.tomas.95@gmail.com>
2024-03-26 12:07:32 +0100

    Clicking buttons

--- src/Section_02/ClickingButons.java
+++ src/Section_02/ClickingButons.java
@@ -1,16 +1,15 @@
-package Section_01;
+package Section_02;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SelectingByLinkText {
+public class ClickingButons {
     public static void main(String[] args) {
         System.setProperty("webdriver.driver.chrome", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://automationtesting.co.uk");
-        driver.findElement(By.linkText("ACTIONS")).click(); //ACTIONS need to be capitalized because it selects an actual text on webpage
-
+        driver.get("https://automationtesting.co.uk/buttons.html");
+        driver.findElement(By.cssSelector("#btn_three")).click();
     }
 }
