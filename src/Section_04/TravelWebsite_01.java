2c87576d53aeb03e818e685191a3800873204adb
Sloth73 <Pichl.tomas.95@gmail.com>
2024-04-04 11:57:21 +0200

    Test for selecting a destination from list od choises on easyjet.com

--- src/Section_04/TravelWebsite_01.java
+++ src/Section_04/TravelWebsite_01.java
@@ -1,13 +1,29 @@
 package Section_04;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class TravelWebsite_01 {
     public static void main(String[] args) {
         System.setProperty("webdriver.driver.chrome", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://automationtesting.co.uk/loader.html");
+        driver.get("https://www.easyjet.com/cs");
+        driver.findElement(By.id("ensRejectAll")).click();
+        driver.findElement(By.cssSelector("input[name='origin']")).click(); // If the element is not clicked first send keys continue to input characters after placeholder
+        driver.findElement(By.cssSelector("input[name='origin']")).sendKeys("Londýn");
+        List<WebElement> origins = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#ui-id-1 li>a>span")));
+        for(WebElement origin:origins) {
+            if(origin.getText().contains("Luton")) {
+                origin.click();
+            }
+        }
     }
 }
