1a2af162c5d9db96d9f90312511730d6b25e1c3d
Sloth73 <Pichl.tomas.95@gmail.com>
2024-04-11 21:36:11 +0200

    Testing XML file

---
+++ src/Section_05/TestingXML.java
@@ -0,0 +1,14 @@
+package Section_05;
+
+import org.junit.Test;
+
+public class TestingXML {
+    @Test
+    public void method1 () {
+        System.out.println("This is method 1");
+    }
+    @Test
+    public void method2 () {
+        System.out.println("This is method 2");
+    }
+}
---
+++ src/Section_05/TestingXML02.java
@@ -0,0 +1,14 @@
+package Section_05;
+
+import org.junit.Test;
+
+public class TestingXML02 {
+    @Test
+    public void method3 () {
+        System.out.println("This is method 3");
+    }
+    @Test
+    public void method4 () {
+        System.out.println("This is method 4");
+    }
+}
